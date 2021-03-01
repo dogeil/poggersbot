2d175c6ba9f94e4ac7731825ac45d183d637a63e
poggersdude <ian.mourao69@gmail.com>
2021-03-01 12:10:17 +0000

    among us

--- Cogs/Misc.py
+++ Cogs/Misc.py
@@ -12,8 +12,11 @@ class Misc(commands.Cog):
 			return
 		else:
 			await ctx.send(error)
-
-	@commands.command(help="Ask the 8ball a question", aliases=["8ball"])
+			
+	@commands.command()
+	async def amongus(self, ctx):
+		await ctx.send("https://media.discordapp.net/attachments/741804760239308873/815915479705190406/16145997448175654420613429936671.png")
+	@commands.command(help="Ask the 8ball a question", aliases=["8			ball"])
 	async def ball(self, ctx, *, question=None):
 		if(question!=None):
 			responses = ["It is certain.","It is decidedly so.","Without a doubt.","Yes – definitely.","You may rely on it.","As I see it, yes.","Most likely.","Outlook good.","Yes.","Signs point to yes.","Reply hazy, try again.","Ask again later.","Better not tell you now.","Cannot predict now.","Concentrate and ask again.","Don't count on it.","My reply is no.","My sources say no.","Outlook not so good.","Very doubtful."]
