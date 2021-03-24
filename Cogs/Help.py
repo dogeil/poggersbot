acab2c2a4c3d4fa2cc04ccdf0be72715b504db0d
poggersdude <ian.mourao69@gmail.com>
2021-03-24 12:44:22 +0000

    t

--- Cogs/Help.py
+++ Cogs/Help.py
@@ -63,8 +63,8 @@ class Help(commands.Cog):
 		att2names=["secondary attack","secondary att","secondary a","sec attack","sec att","sec a","s attack","s att","s a","attack2","att2","a2","2"]
 		await log(ctx)
 		embed = discord.Embed(title="Engineer\'s Fighting Club", description="These are the basic fighting commands that can be used by every class", timestamp=datetime.datetime.utcnow(), color=discord.Color.red())
-		embed.add_field(name="attack", value=f"normal attack that has average damage and always hits (damage=15-20, aliases={str(attnames)})")
-		embed.add_field(name="secondary attack", value=f"slightly different attack that deals higher damage but only has a 50% chance to hit (damage=35-40, aliases={str(att2names)})")
+		embed.add_field(name="attack", value=f"**normal attack that has average damage and always hits** (damage=15-20, aliases={str(attnames)})")
+		embed.add_field(name="secondary attack", value=f"**slightly different attack that deals higher damage but only has a 50% chance to hit** (damage=35-40, aliases={str(att2names)})")
 		await ctx.send(embed=embed)
 
 def setup(bot):
